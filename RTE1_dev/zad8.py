8c4504ba2de68b9364e2d9538d6f75df0fabe47e
Jan Zyśko <jz306065@blue08.mimuw.edu.pl>
2018-04-23 13:42:15 +0200

    zadanie8

---
+++ RTE1_dev/zad8.py
@@ -0,0 +1,21 @@
+from nltk.corpus import wordnet as wn
+import nltk
+import xml.etree.ElementTree as ET
+from bs4 import BeautifulSoup
+from nltk.tree import Tree
+
+with open('dev.xml', 'r') as myfile:
+  content_html = myfile.read()
+
+soup = BeautifulSoup(content_html, 'lxml')
+
+nltk.download('maxent_ne_chunker')
+nltk.download('punkt')
+nltk.download('averaged_perceptron_tagger')
+
+for node in soup.findAll('t',text=True) + soup.findAll('h',text=True):
+	print(str(node.contents[0]))
+	text = nltk.word_tokenize(str(node.contents[0]))
+	tagged_text = nltk.pos_tag(text)
+	for el in nltk.ne_chunk(tagged_text, binary=True):
+		print(el)
