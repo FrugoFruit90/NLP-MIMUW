5f39a9abdc23840d433f5e6403964c1e7e0f2d29
Jan Zyśko <jan.zysko@pracuj.pl>
2018-05-05 13:21:34 +0200

    zadania 8

--- RTE1_dev/zad8.py
+++ RTE1_dev/zad8.py
@@ -5,21 +5,24 @@ from bs4 import BeautifulSoup
 from nltk.tree import Tree
 
 with open('dev.xml', 'r') as myfile:
-  content_html = myfile.read()
+    content_html = myfile.read()
 
 soup = BeautifulSoup(content_html, 'lxml')
 
-nltk.download('maxent_ne_chunker')
-nltk.download('punkt')
-nltk.download('averaged_perceptron_tagger')
+# nltk.download('wordnet')
+# nltk.download('words')
+# nltk.download('maxent_ne_chunker')
+# nltk.download('punkt')
+# nltk.download('averaged_perceptron_tagger')
 
-for node in soup.findAll('t',text=True) + soup.findAll('h',text=True):
-	print(str(node.contents[0]))
-	text = nltk.word_tokenize(str(node.contents[0]))
-	tagged_text = nltk.pos_tag(text)
-	for el in nltk.ne_chunk(tagged_text, binary=True):
-		print(el)
-		if len(el) == 1:
-			print(el, wn.synsets(el)[0])
-		else:
-			
+for node in soup.findAll('t', text=True) + soup.findAll('h', text=True):
+    print(str(node.contents[0]))
+    text = nltk.word_tokenize(str(node.contents[0]))
+    tagged_text = nltk.pos_tag(text)
+    for el in nltk.ne_chunk(tagged_text, binary=True):
+        print(wn.synsets(el[0][0]))
+
+        # if len(el) == 1:
+        #     print(el, wn.synsets(el)[0])
+        # else:
+        #     print("trololo")
