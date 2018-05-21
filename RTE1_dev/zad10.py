20ff5f9862e5a9bf42606195e79f54fc51d58cdd
Jan Zyśko <jan.zysko@pracuj.pl>
2018-05-21 08:45:49 +0200

    format

--- RTE1_dev/zad10.py
+++ RTE1_dev/zad10.py
@@ -5,7 +5,7 @@ from bs4 import BeautifulSoup
 from nltk.tree import Tree
 
 with open('dev.xml', 'r') as myfile:
-  content_html = myfile.read()
+    content_html = myfile.read()
 
 soup = BeautifulSoup(content_html, 'lxml')
 
@@ -13,15 +13,12 @@ nltk.download('maxent_ne_chunker')
 nltk.download('punkt')
 nltk.download('averaged_perceptron_tagger')
 
-texts = soup.findAll('t',text=True)
-hypotheses = soup.findAll('h',text=True)
+texts = soup.findAll('t', text=True)
+hypotheses = soup.findAll('h', text=True)
 
 for i in range(len(texts)):
-	text = nltk.word_tokenize(str(texts[i].contents[0]))
-	tagged_text = nltk.pos_tag(text)
-	hyp = nltk.word_tokenize(str(hypotheses[i].contents[0]))
-	tagged_hyp = nltk.pos_tag(hyp)
-	print(tagged_text, tagged_hyp)
-
-
-			
+    text = nltk.word_tokenize(str(texts[i].contents[0]))
+    tagged_text = nltk.pos_tag(text)
+    hyp = nltk.word_tokenize(str(hypotheses[i].contents[0]))
+    tagged_hyp = nltk.pos_tag(hyp)
+    print(tagged_text, tagged_hyp)
