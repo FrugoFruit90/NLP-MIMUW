6102dcd7502980c8c484e88d67b56c62b42117fd
Jan Zyśko <jan.zysko@pracuj.pl>
2018-05-22 10:57:40 +0200

    fix

--- RTE1_dev/zad8.py
+++ RTE1_dev/zad8.py
@@ -13,6 +13,18 @@ soup = BeautifulSoup(content_html, 'lxml')
 # nltk.download('punkt')
 # nltk.download('averaged_perceptron_tagger')
 
+nouns = 0
+nouns_found = 0
+
+verbs = 0
+verbs_found = 0
+
+adjs = 0
+adjs_found = 0
+
+advs = 0
+advs_found = 0
+
 for node in soup.findAll('t', text=True) + soup.findAll('h', text=True):
     print(str(node.contents[0]))
     text = nltk.word_tokenize(str(node.contents))
@@ -26,7 +38,46 @@ for node in soup.findAll('t', text=True) + soup.findAll('h', text=True):
                 raise ValueError
         else:
             try:
+                if tagged_word[1].startswith('NN'):
+                    nouns += 1
+                    if len(wn.synsets(tagged_word[0])) > 0:
+                        nouns_found += 1
+                elif tagged_word[1].startswith('VB'):
+                    verbs += 1
+                    if len(wn.synsets(tagged_word[0])) > 0:
+                        verbs_found += 1
+                elif tagged_word[1].startswith('JJ'):
+                    adjs += 1
+                    if len(wn.synsets(tagged_word[0])) > 0:
+                        adjs_found += 1
+                elif tagged_word[1].startswith('RB') or tagged_word[1] == 'WRB':
+                    advs += 1
+                    if len(wn.synsets(tagged_word[0])) > 0:
+                        advs_found += 1
+            except IndexError:
+                pass
+            try:
                 print(tagged_word, wn.synsets(tagged_word[0])[0].lemma_names(),
                       wn.synsets(tagged_word[0])[0].hypernyms())
             except IndexError:
                 print(tagged_word)
+
+print('\nNouns:')
+print(nouns)
+print(nouns_found)
+print(nouns_found/nouns)
+
+print('\nVerbs:')
+print(verbs)
+print(verbs_found)
+print(verbs_found/verbs)
+
+print('\nAdjectives:')
+print(adjs)
+print(adjs_found)
+print(adjs_found/adjs)
+
+print('\nAdverbs:')
+print(advs)
+print(advs_found)
+print(advs_found/advs)
