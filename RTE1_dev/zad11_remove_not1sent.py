977efe37a17950c269fef100483b457d11f8e02a
Jan Zyśko <jan.zysko@pracuj.pl>
2018-06-07 12:42:14 +0200

    1 sentence

--- RTE1_dev/zad11_remove_not1sent.py
+++ RTE1_dev/zad11_remove_not1sent.py
@@ -1,8 +1,27 @@
 
-values = open('data/values.txt', 'r')
-texts = open('data/texts.txt', 'r')
-texts_1_sent = open('data/texts_1_sent.txt', 'r')
+values_file = open('data/values.txt', 'r')
+texts_file = open('data/texts.txt', 'r')
+texts_1_sent_file = open('data/texts_1_sent.txt', 'r')
+values_1_sent_file = open('data/values_1_sent.txt', 'w')
 
-values_1_sent = open('data/values_1_sent')
 
-f.readline()
+while True:
+    curr_text = texts_file.readline()
+    curr_text_1_sent = texts_1_sent_file.readline()
+    curr_value = values_file.readline()
+
+    if curr_text == '':
+        break
+
+    while True:
+        if curr_text == curr_text_1_sent:
+            values_1_sent_file.write(curr_value)
+            break
+        else:
+            curr_text = texts_file.readline()
+            curr_value = values_file.readline()
+
+values_file.close()
+texts_file.close()
+texts_1_sent_file.close()
+values_1_sent_file.close()
