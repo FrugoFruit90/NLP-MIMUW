951598e164d25e24057d2d024812de532c376a11
Jan Zyśko <jan.zysko@pracuj.pl>
2018-06-07 12:00:55 +0200

    data

---
+++ RTE1_dev/zad11_remove_not1sent.py
@@ -0,0 +1,8 @@
+
+values = open('data/values.txt', 'r')
+texts = open('data/texts.txt', 'r')
+texts_1_sent = open('data/texts_1_sent.txt', 'r')
+
+values_1_sent = open('data/values_1_sent')
+
+f.readline()
