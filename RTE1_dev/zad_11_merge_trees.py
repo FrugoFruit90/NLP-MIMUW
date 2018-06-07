3cf0bb297bdbec1bb57aa741fd48072553d9705f
Jan Zyśko <jan.zysko@pracuj.pl>
2018-06-07 13:35:41 +0200

    learning_data

---
+++ RTE1_dev/zad_11_merge_trees.py
@@ -0,0 +1,11 @@
+texts = open('data/texts_parsed.txt', 'r').readlines()
+values = open('data/values_1_sent.txt', 'r').readlines()
+hypotheses = open('data/hypotheses_parsed.txt', 'r').readlines()
+
+values = ['1' if x == 'TRUE\n' else '0' for x in values]
+
+
+with open('data/learning.txt', 'w') as file:
+    for i in range(len(texts)):
+        line = values[i] + ' |BT| ' + texts[i][:-1] + ' |BT| ' + hypotheses[i][:-1] + ' |ET|\n'
+        file.write(line)
